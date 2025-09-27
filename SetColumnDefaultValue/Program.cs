ac47928ed6f0683a8c0ee83b17753e6ffbed9fc0
Simmon Cui <cuicheng11165@outlook.com>
2025-09-27 15:32:36 +0800

    Add more test

--- SetColumnDefaultValue/Program.cs
+++ SetColumnDefaultValue/Program.cs
@@ -1,41 +1,12 @@
-﻿
+using System;
 
-
-
- 
-using Microsoft.SharePoint.Client;
-using System.Text;
-
-
-ClientContext context = new ClientContext("https://bigapp.sharepoint.com/teams/Teams202504221153");
-
- 
-
-var list = context.Web.Lists.GetByTitle("lib6");
-var column = list.Fields.GetByTitle("m1");
-
-context.Load(column);
-context.ExecuteQuery();
-
-var termName = "C";
-var termId = "23f5a117-458e-44fa-ac24-ff1fe1926054";
-var defaultValueString = $"-1;#{termName}|{termId}";
-
-column.DefaultValue = defaultValueString;
-
-column.Update();
-
-var newAddedFile = list.RootFolder.Files.Add(new FileCreationInformation()
+namespace SetColumnDefaultValue
 {
-    Url = "AddFileWithBytes.txt",
-    Overwrite = true,
-    Content = Encoding.UTF8.GetBytes("TestDocumentContent")
-});
-
-context.ExecuteQuery();
-
-
-
-
-
-Console.ReadLine();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetColumnDefaultValueScenarios.SetDefaultValueAndAddFile();
+        }
+    }
+}
---
+++ SetColumnDefaultValue/SetColumnDefaultValueScenarios.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Text;
+using Microsoft.SharePoint.Client;
+
+namespace SetColumnDefaultValue
+{
+    internal static class SetColumnDefaultValueScenarios
+    {
+        internal static void SetDefaultValueAndAddFile()
+        {
+            using ClientContext context = new ClientContext("https://bigapp.sharepoint.com/teams/Teams202504221153");
+
+            var list = context.Web.Lists.GetByTitle("lib6");
+            var column = list.Fields.GetByTitle("m1");
+
+            context.Load(column);
+            context.ExecuteQuery();
+
+            var termName = "C";
+            var termId = "23f5a117-458e-44fa-ac24-ff1fe1926054";
+            var defaultValueString = $"-1;#{termName}|{termId}";
+
+            column.DefaultValue = defaultValueString;
+            column.Update();
+
+            var newAddedFile = list.RootFolder.Files.Add(new FileCreationInformation
+            {
+                Url = "AddFileWithBytes.txt",
+                Overwrite = true,
+                Content = Encoding.UTF8.GetBytes("TestDocumentContent")
+            });
+
+            context.ExecuteQuery();
+
+            Console.ReadLine();
+        }
+    }
+}
